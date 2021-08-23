e2e57a3f92f90b08ea8918ae7ed6a0c83465fe15
Nayan Shah <196625+nayanshah@users.noreply.github.com>
2021-08-23 02:30:38 -0700

    Reduce timeout at end of install command (#52)
    * Reduce timeout at end of install to 100ms
    
    Speeds up no-op times by reducing delay to 100ms. #50
    
    * Update install.js

--- src/cli/commands/install.js
+++ src/cli/commands/install.js
@@ -1227,7 +1227,7 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
   // Let time for all the output to be printed and then hard exit.
   // This fixes an issue where a hanging request would keep node alive.
   // eslint-disable-next-line no-process-exit
-  setTimeout(() => process.exit(process.exitCode || 0), 1000);
+  setTimeout(() => process.exit(process.exitCode || 0), 50);
 }
 
 export async function wrapLifecycle(config: Config, flags: Object, factory: () => Promise<void>): Promise<void> {
